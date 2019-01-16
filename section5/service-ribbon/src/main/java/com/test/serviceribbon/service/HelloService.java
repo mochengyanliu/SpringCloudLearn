caf761e88c3e7257c48a8e0c18859ae23fe91c5f
lijun <lijun01@anji-allways.com>
2019-01-16 14:38:22 +0800

    第五节: 路由网关（zuul）

---
+++ section5/service-ribbon/src/main/java/com/test/serviceribbon/service/HelloService.java
@@ -0,0 +1,21 @@
+package com.test.serviceribbon.service;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class HelloService {
+    @Autowired
+    RestTemplate restTemplate;
+
+    @HystrixCommand(fallbackMethod = "helloError")
+    public String helloService(String name){
+        return restTemplate.getForObject("http://eureka-client/hello?name=" + name,String.class);
+    }
+
+    public String helloError(String name){
+        return "hello,"+name+",sorry,error!";
+    }
+}
